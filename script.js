d38df53b48a21090a3386fced5ffa1327a6a9c19
Pratiksha Ranbaware <pratiksharanbaware01@gmail.com>
2026-07-08 18:06:06 +0530

    Added style.css and script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const button = document.querySelector("button");
+    const searchBox = document.querySelector("input");
+
+    button.addEventListener("click", function () {
+
+        let scheme = searchBox.value.trim();
+
+        if (scheme === "") {
+            alert("Please enter a scheme name.");
+        } else {
+            alert("You searched for: " + scheme);
+        }
+
+    });
+
+});
